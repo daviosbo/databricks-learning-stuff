a6ddef276012a90a49a84619b95c28fd6f848a3c
David Osborne <52266939+daviosbo@users.noreply.github.com>
2022-03-03 11:37:05 +1030

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+# MAGIC %md Python test NB
+
+# COMMAND ----------
+
+from platform import machine, python_version, platform
+print(machine());
+print(python_version());
+print(platform());
+
+# COMMAND ----------
+
+print("hello world")
